046a601d34b3bcc16eebac661bebe14e3f05e85e
wangqinqin <wangqinqin@wangqinqin.local>
2017-07-25 09:23:37 +0800

    [dev] 添加图片处理

--- src/containers/login.js
+++ src/containers/login.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { loginFetch } from '../actions/login/login.js'
-import { browserHistory , hashHistory, Link} from 'react-router'
+import { hashHistory, Link} from 'react-router'
 import $ from 'jquery'
 
 
