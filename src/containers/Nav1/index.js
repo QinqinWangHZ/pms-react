137e0e96e35236ed3956db41661f942c5bf06941
caoxf <caoxifeng@lztogether.com>
2017-07-27 18:11:32 +0800

    添加

--- src/containers/Nav1/index.js
+++ src/containers/Nav1/index.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 import './style.scss';
 
-export default class App extends Component {
+export default class Nav1 extends Component {
   render() {
     return (
         <div>
--- src/routers2.js
+++ src/routers2.js
@@ -6,6 +6,7 @@ import Welcome from './containers/Welcome/index.js';
 import Dashboard from './containers/Dashboard/index.js';
 import Nav1 from './containers/Nav1/index.js';
 import Nav2 from './containers/Nav2/index.js';
+import Nav2_Order from './containers/Nav2/subpage/Order/index.js';
 import NotFound from './containers/NotFound/index.js';
 
 const routes = (
@@ -14,8 +15,11 @@ const routes = (
       <Route path="welcome" component={Welcome}/>
       <Route path="dashboard" component={Dashboard}>
         <IndexRedirect to="nav1"/>
+        <Route path="nav2" component={Nav2}>
+            <IndexRedirect to="nav2_order"/>
+            <Route path="nav2_order" component={Nav2_Order} />
+        </Route>
         <Route path="nav1" component={Nav1} />
-        <Route path="nav2" component={Nav2} />
       </Route>
       <Route path="*" component={NotFound}/>
     </Route>
