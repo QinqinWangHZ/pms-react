7c4913c3f0762ee3df5782f8827acccad7d92686
wangqinqin <wangqinqin@wangqinqin.local>
2017-07-27 12:07:09 +0800

    [dev] 登录

---
+++ src/containers/login/sigin.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import $ from 'jquery';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+import { loginFetch } from '../../actions/login/login';
+import hashHistory from '../../history';
+
+
+// 测试引入jquery
+// $('#test').html('两只小蜜蜂');
+
+class Login extends Component {
+  login() {
+    const { dispatch } = this.props;
+
+    // validate
+    if (!this.username.value) {
+      alert('请输入用户名');
+      return;
+    } else if (!this.password.value) {
+      alert('请输入密码');
+      return;
+    }
+    const page = this.props.params.page;
+
+    dispatch(loginFetch(this.username.value, this.password.value, () => {
+      hashHistory.push(page ? page.replace(/-/g, '/') : '/date');
+    }));
+  }
+
+  render() {
+    return (
+      <div className="loginC">
+        <div className="container">
+          <div className="innerbox">
+            <div className="login clearfix">
+              <div className="logForm">
+                <ul>
+                  <li className="formgroup">
+                    <div className="forminput">
+                      <span className="login-form-username" /><input className="input" type="text" name="user_name" placeholder="请输入手机号" maxLength="11" ref={(username) => { this.username = username; }} />
+                    </div>
+                  </li>
+                  <li className="formgroup">
+                    <div className="forminput">
+                      <span className="login-form-password" /><input id="password" className="input" type="password" name="password" placeholder="请输入密码" ref={(password) => { this.password = password; }} />
+                    </div>
+                  </li>
+                  <li className="formgroup">
+                    <em className="fl"><input id="rememberMe" name="rememberMe" type="checkbox" />&nbsp;记住用户名</em>
+                    <em className="fr"><a href="forget_password.html">忘记密码？</a></em></li>
+                  <li className="formgroup btn">
+                    <button id="submit_login" type="submit" className="ui-btn ui-btn-big" onClick={() => this.login()}> <span>立即登录</span> </button>
+                  </li>
+                  <li className="formgroup">
+                    <em className="fr">没有账号？ <Link to="register">立即注册</Link></em>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+function map(state) {
+  console.log('qinqin');
+  console.log(state);
+  return {
+  };
+}
+
+export default connect(map)(Login);
--- src/routers.js
+++ src/routers.js
@@ -2,6 +2,7 @@
 import React, {Component, PropTypes} from 'react';
 import { Router, Route, Redirect, IndexRoute, browserHistory, hashHistory, IndexRedirect } from 'react-router';
 import Home from './containers/home.js';
+import App from './components/App';
 
 // 按需加载
 const TestCenter = (location, cb) => {
@@ -39,13 +40,45 @@ const date = (location, cb) => {
 }
 
 const routes = (
-    <Route path="/" component={Home}>
-      <Route path="repos" getComponent={TestCenter}/>
-      <Route path="login" getComponent={login}/>
-      <Route path="PieReact" getComponent={PieReact}/>
-      <Route path="test" getComponent={test}/>
-      <Route path="date" getComponent={date}/>
-    </Route>
+  <Route path="/" component={App}>
+    <IndexRedirect to="signin"/>
+    <Route path="signin"
+      breadcrumbName="登陆"
+      getComponent={(nextState, cb) => {
+       require.ensure([], require => {
+         cb(null, require('./containers/login/sigin').default)
+       }, 'signin')
+      }}
+    />
+    <Route path="signup"
+      breadcrumbName="注册"
+      getComponent={(nextState, cb) => {
+       require.ensure([], require => {
+         cb(null, require('./containers/login/register').default)
+       }, 'signup')
+      }}
+    />
+    <Route path="forget"
+      breadcrumbName="忘记密码"
+      getComponent={(nextState, cb) => {
+       require.ensure([], require => {
+         cb(null, require('./containers/login/forget').default)
+       }, 'forget')
+      }}
+    />
+    <Route path="date" getComponent={date}/>
+    <Route path="repos" getComponent={TestCenter}/>
+    <Route path="login" getComponent={login}/>
+    <Route path="PieReact" getComponent={PieReact}/>
+    <Route path="test" getComponent={test}/>
+  </Route>
+    // <Route path="/" component={Home}>
+    //   <Route path="repos" getComponent={TestCenter}/>
+    //   <Route path="login" getComponent={login}/>
+    //   <Route path="PieReact" getComponent={PieReact}/>
+    //   <Route path="test" getComponent={test}/>
+    //   <Route path="date" getComponent={date}/>
+    // </Route>
 )
 
 export default routes;
